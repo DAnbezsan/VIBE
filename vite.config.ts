152e3801adc7ea8efd0af197d6a2d0a5089583fc
DAnbezsan <2017danilosantos@gmail.com>
2026-02-07 17:48:19 -0300

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/VIBE/', // MUITO IMPORTANTE: deve ser exatamente o nome do seu repositório no GitHub
+  base: './', // Essencial para deploy manual via branch (caminhos relativos)
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: false,
     emptyOutDir: true
   }
 });
+
